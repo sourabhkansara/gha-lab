07721d1f039911ac5c730d37ed8bb50d8e841311
sourabhkansara621 <sourabh.k.kansara@lumen.com>
2026-06-16 17:16:14 +0530

    init

---
+++ src/main/java/com/example/gha_lab/GhaLabApplication.java
@@ -0,0 +1,14 @@
+package com.example.gha_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhaLabApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
